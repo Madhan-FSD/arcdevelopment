c5238b4db55c287bf138b0fcfdfa4cedf8fd2123
Madhan-FSD <jobsformadhan@gmail.com>
2025-10-25 17:54:52 +0530

    90% of work on header is completed for both mobile and desktop

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,21 +2,22 @@ import { ThemeProvider } from "@mui/material/styles";
 import Header from "./ui/Header";
 import theme from "./ui/theme";
 import { Routes, Route } from "react-router-dom";
+import { Box } from "@mui/material";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <Header />
       <Routes>
-        <Route path="/" element={<div>Home</div>} />
-        <Route path="/services" element={<div>Services</div>} />
-        <Route path="/customsoftwar" element={<div>Custom Software</div>} />
-        <Route path="/mobileapps" element={<div>Mobile Apps</div>} />
-        <Route path="/websites" element={<div>Websites</div>} />
-        <Route path="/revolution" element={<div>Revolution</div>} />
-        <Route path="/about" element={<div>About Us</div>} />
-        <Route path="/contact" element={<div>Contact Us</div>} />
-        <Route path="/estimate" element={<div>Estimate</div>} />
+        <Route path="/" element={<Box>Home</Box>} />
+        <Route path="/services" element={<Box>Services</Box>} />
+        <Route path="/customsoftware" element={<Box>Custom Software</Box>} />
+        <Route path="/mobileapps" element={<Box>Mobile Apps</Box>} />
+        <Route path="/websites" element={<Box>Websites</Box>} />
+        <Route path="/revolution" element={<Box>Revolution</Box>} />
+        <Route path="/about" element={<Box>About Us</Box>} />
+        <Route path="/contact" element={<Box>Contact Us</Box>} />
+        <Route path="/estimate" element={<Box>Estimate</Box>} />
       </Routes>
     </ThemeProvider>
   );
--- src/components/ui/Header.jsx
+++ src/components/ui/Header.jsx
@@ -1,21 +1,49 @@
-import React, { Fragment, useEffect, useState } from "react";
+import React, { Fragment, useEffect, useMemo, useState } from "react";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import useScrollTrigger from "@mui/material/useScrollTrigger";
 import { styled } from "@mui/material/styles";
 import { Link } from "react-router-dom";
-import { Button, Tab, Tabs } from "@mui/material";
+import {
+  Button,
+  Menu,
+  MenuItem,
+  Paper,
+  Tab,
+  Tabs,
+  useMediaQuery,
+  SwipeableDrawer,
+  IconButton,
+  List,
+  ListItem,
+  ListItemText,
+  ListItemButton,
+} from "@mui/material";
+import { useTheme } from "@mui/material/styles";
+import MenuIcon from "@mui/icons-material/Menu";
 
 import logo from "../../assets/logo.svg";
 
 const ToolBarWithMargin = styled(Toolbar)(({ theme }) => ({
   ...theme.mixins.toolbar,
   marginBottom: "3em",
+  [theme.breakpoints.down("md")]: {
+    marginBottom: "2em",
+  },
+  [theme.breakpoints.down("xs")]: {
+    height: "1.25em",
+  },
 }));
 
-const LogoImage = styled("img")({
+const LogoImage = styled("img")(({ theme }) => ({
   height: "8em",
-});
+  [theme.breakpoints.down("md")]: {
+    height: "7em",
+  },
+  [theme.breakpoints.down("xs")]: {
+    height: "5.5em",
+  },
+}));
 
 const TabsContainer = styled(Tabs)({
   marginLeft: "auto",
@@ -42,6 +70,65 @@ const LogoContainer = styled(Button)({
   },
 });
 
+const StyledMenu = styled(Menu)(({ theme }) => ({
+  "& .MuiPaper-root": {
+    backgroundColor: theme.palette.common.blue,
+    color: theme.palette.common.white,
+    borderRadius: 0,
+    zIndex: theme.zIndex.drawer + 1,
+  },
+}));
+
+const StyledMenuItem = styled(MenuItem)(({ theme }) => ({
+  "&.Mui-selected": {
+    backgroundColor: `${theme.palette.primary.dark} !important`,
+  },
+  ...theme.typography.tab,
+}));
+
+const DrawerIconContainer = styled(IconButton)({
+  marginLeft: "auto",
+  "&:hover": {
+    backgroundColor: "transparent",
+  },
+});
+
+const DrawerIcon = styled(MenuIcon)({
+  height: "50px",
+  width: "50px",
+});
+
+const StyledDrawer = styled(SwipeableDrawer)(({ theme }) => ({
+  "& .MuiPaper-root": {
+    backgroundColor: theme.palette.common.blue,
+    color: theme.palette.common.white,
+    borderRadius: 0,
+  },
+}));
+
+const StyledListItemText = styled(ListItemText)(({ theme }) => ({
+  ...theme.typography.tab,
+  color: "white",
+}));
+
+const StyledFreeEstimateButton = styled(Button)(({ theme }) => ({
+  ...theme.typography.tab,
+  color: "white",
+  backgroundColor: theme.palette.common.orange,
+}));
+
+const StyledListItemButton = styled(ListItemButton)(({ theme }) => ({
+  opacity: 0.7,
+  "&.Mui-selected": {
+    backgroundColor: `${theme.palette.primary.dark} !important`,
+    opacity: 1,
+  },
+}));
+
+const StyledAppBar = styled(AppBar)(({ theme }) => ({
+  zIndex: theme.zIndex.drawer + 1,
+}));
+
 function ElevationScroll(props) {
   const { children } = props;
   const trigger = useScrollTrigger({
@@ -57,67 +144,224 @@ function ElevationScroll(props) {
 }
 
 export default function Header() {
+  const theme = useTheme();
+  const iOS =
+    typeof navigator !== "undefined" &&
+    /iPad|iPhone|iPod/.test(navigator.userAgent);
+  const matches = useMediaQuery(theme.breakpoints.down("md"));
+
+  const [openDrawer, setOpenDrawer] = useState(false);
   const [value, setValue] = useState(0);
+  const [anchorEl, setAnchorEl] = useState(null);
+  const [openMenu, setOpenMenu] = useState(false);
+  const [selectedIndex, setSelectedIndex] = useState(0);
 
-  const handleChange = (e, value) => {
-    setValue(value);
-  };
+  const menuOptions = useMemo(
+    () => [
+      { name: "Services", link: "/services", activeIndex: 1, selectedIndex: 0 },
+      {
+        name: "Custom Software Development",
+        link: "/customsoftware",
+        activeIndex: 1,
+        selectedIndex: 1,
+      },
+      {
+        name: "Mobile App Development",
+        link: "/mobileapps",
+        activeIndex: 1,
+        selectedIndex: 2,
+      },
+      {
+        name: "Websites Development",
+        link: "/websites",
+        activeIndex: 1,
+        selectedIndex: 3,
+      },
+    ],
+    []
+  );
 
-  const pathToValue = {
-    "/": 0,
-    "/services": 1,
-    "/revolution": 2,
-    "/about": 3,
-    "/contact": 4,
-    "/estimate": 5,
-  };
+  const routes = useMemo(
+    () => [
+      { name: "Home", link: "/", activeIndex: 0 },
+      {
+        name: "Services",
+        link: "/services",
+        activeIndex: 1,
+        ariaOwns: anchorEl ? "simple-menu" : undefined,
+        ariaPopup: anchorEl ? "true" : undefined,
+        onMouseOver: (e) => handlClick(e),
+      },
+      { name: "The Revolution", link: "/revolution", activeIndex: 2 },
+      { name: "About Us", link: "/about", activeIndex: 3 },
+      { name: "Contact Us", link: "/contact", activeIndex: 4 },
+    ],
+    [anchorEl]
+  );
 
   useEffect(() => {
-    const newValue = pathToValue[window.location.pathname];
-    if (newValue !== undefined && newValue !== value) {
-      setValue(newValue);
-    }
-  }, [value]);
+    [...menuOptions, ...routes].forEach((route) => {
+      switch (window.location.pathname) {
+        case `${route.link}`:
+          if (value !== route.activeIndex) {
+            setValue(route.activeIndex);
+            if (route.selectedIndex && route.selectedIndex !== selectedIndex) {
+              setSelectedIndex(route.selectedIndex);
+            }
+          }
+          break;
+        default:
+          break;
+      }
+    });
+  }, [menuOptions, routes, selectedIndex, value]);
+
+  const handleChange = (e, newValue) => {
+    setValue(newValue);
+  };
+
+  const handlClick = (e) => {
+    setAnchorEl(e.currentTarget);
+    setOpenMenu(true);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+    setOpenMenu(false);
+  };
+
+  const handleMenuItemClick = (e, i) => {
+    setSelectedIndex(i);
+    setAnchorEl(null);
+    setOpenMenu(false);
+  };
+
+  const tabs = (
+    <Fragment>
+      <TabsContainer
+        indicatorColor="primary"
+        textColor="secondary"
+        value={value}
+        onChange={handleChange}
+        aria-label="header tabs"
+      >
+        {routes.map((route) => (
+          <StyledTab
+            aria-owns={route?.ariaOwns}
+            aria-haspopup={route?.ariaPopup}
+            onMouseOver={route?.onMouseOver}
+            key={route.link}
+            component={Link}
+            to={route.link}
+            label={route?.name}
+          />
+        ))}
+      </TabsContainer>
+      <StyledButton variant="contained" color="secondary">
+        Free Estimate
+      </StyledButton>
+      <StyledMenu
+        id="simple-menu"
+        onClose={handleClose}
+        open={openMenu}
+        anchorEl={anchorEl}
+        slotProps={{
+          list: { onMouseLeave: handleClose },
+        }}
+        elevation={0}
+        keepMounted
+      >
+        {menuOptions.map((option, index) => (
+          <StyledMenuItem
+            component={Link}
+            to={option?.link}
+            key={`${option?.name}_${index}`}
+            onClick={(e) => {
+              handleMenuItemClick(e, index);
+              setValue(1);
+              handleClose();
+            }}
+            selected={index === selectedIndex && value === 1}
+          >
+            {option?.name}
+          </StyledMenuItem>
+        ))}
+      </StyledMenu>
+    </Fragment>
+  );
+
+  const drawer = (
+    <Fragment>
+      <StyledDrawer
+        disableBackdropTransition={!iOS}
+        disableDiscovery={iOS}
+        open={openDrawer}
+        onClose={() => setOpenDrawer(false)}
+        onOpen={() => setOpenDrawer(true)}
+      >
+        <ToolBarWithMargin />
+        <List disablePadding>
+          {routes.map((route) => (
+            <ListItem
+              key={route.link}
+              disablePadding
+              divider
+              component={Link}
+              to={route.link}
+            >
+              <StyledListItemButton
+                selected={value === route.activeIndex}
+                onClick={() => {
+                  setOpenDrawer(false);
+                  setValue(route.activeIndex);
+                }}
+              >
+                <StyledListItemText disableTypography>
+                  {route?.name}
+                </StyledListItemText>
+              </StyledListItemButton>
+            </ListItem>
+          ))}
+          <ListItem disablePadding divider component={Link} to="/estimate">
+            <StyledFreeEstimateButton
+              fullWidth
+              onClick={() => {
+                setOpenDrawer(false);
+                setValue(5);
+              }}
+            >
+              <StyledListItemText disableTypography>
+                Free Estimate
+              </StyledListItemText>
+            </StyledFreeEstimateButton>
+          </ListItem>
+        </List>
+      </StyledDrawer>
+      <DrawerIconContainer
+        disableRipple
+        onClick={() => setOpenDrawer(!openDrawer)}
+      >
+        <DrawerIcon />
+      </DrawerIconContainer>
+    </Fragment>
+  );
 
   return (
     <Fragment>
       <ElevationScroll>
-        <AppBar position="fixed">
+        <StyledAppBar position="fixed">
           <Toolbar disableGutters>
             <LogoContainer
               disableRipple
               onClick={() => setValue(0)}
-              LinkComponent={Link}
+              component={Link}
               to="/"
             >
               <LogoImage alt="company logo" src={logo} />
             </LogoContainer>
-            <TabsContainer
-              indicatorColor="primary"
-              textColor="secondary"
-              value={value}
-              onChange={handleChange}
-              aria-label="header tabs"
-            >
-              <StyledTab LinkComponent={Link} to="/" label="Home" />
-              <StyledTab LinkComponent={Link} to="/services" label="Services" />
-              <StyledTab
-                LinkComponent={Link}
-                to="/revolution"
-                label="The Revolution"
-              />
-              <StyledTab LinkComponent={Link} to="/about" label="About Us" />
-              <StyledTab
-                LinkComponent={Link}
-                to="/contact"
-                label="Contact Us"
-              />
-              <StyledButton variant="contained" color="secondary">
-                Free Estimate
-              </StyledButton>
-            </TabsContainer>
+            {matches ? drawer : tabs}
           </Toolbar>
-        </AppBar>
+        </StyledAppBar>
       </ElevationScroll>
       <ToolBarWithMargin />
     </Fragment>
